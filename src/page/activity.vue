90eb4843d0aff9499c74e598db7f0e427d1d651a
Will <pan123033@qq.com>
2017-08-07 16:49:05 +0800

    footer组件构建

---
+++ src/page/activity.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="activity">
+    <h2>活动</h2>
+
+    <common-footer></common-footer>
+  </div>
+</template>
+
+<script>
+  import { XHeader, Swiper, Search } from 'vux'
+  import CommonFooter from '@/components/CommonFooter'
+
+
+  export default {
+    name: 'activity',
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      CommonFooter
+    },
+    methods: {
+
+    },
+    created(){
+      console.log(this.$store.state.count);
+    }
+  }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .logo {
+    max-height: 140%;
+    width: auto;
+    margin-top: -8px;
+  }
+
+  .vux-header {
+    background-color: #fff;
+    box-shadow: 2px 0 12px -6px #aaa;
+    border-bottom: 1px solid #eee;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ Vue.use(Router);
 const routes = [
   {path: "/", redirect: {name: "index"}},
   {path: "/index", component: resolve => require(['@/page/index'], resolve),  name: "index"},
-  {path: "/reserve", component: resolve => require(['@/page/reserve'], resolve),  name: "reserve"}
+  {path: "/reserve", component: resolve => require(['@/page/reserve'], resolve),  name: "reserve"},
+  {path: "/activity", component: resolve => require(['@/page/activity'], resolve),  name: "activity"}
 ];
 
 export default new Router({
